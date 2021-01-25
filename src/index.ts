66170047354955568f4de99e7da9ceea6c792976
VladTzaru <bvanja.87@gmail.com>
2021-01-25 21:47:25 +0100

    Added express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello");
+import express from "express";
+
+const app = express();
+const port = 5000;
+
+app.get("/", (_, res) => {
+  res.send("Hello there");
+});
+
+app.listen(port, () => {
+  console.log(`Listenin at port ${port}`);
+});
