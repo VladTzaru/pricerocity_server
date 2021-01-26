695fb72a0f7341174bd5db9623b177201e6f2d09
VladTzaru <bvanja.87@gmail.com>
2021-01-26 17:18:36 +0100

    Added bodyParser

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,14 @@
+import { Options } from "@mikro-orm/core";
+import { MongoHighlighter } from "@mikro-orm/mongo-highlighter";
+import { Item } from "./entities/Item";
+
+const options: Options = {
+  type: "mongo",
+  entities: [Item],
+  clientUrl: process.env.MONGODB_URI,
+  dbName: "pricerocityDB",
+  highlighter: new MongoHighlighter(),
+  debug: true,
+};
+
+export default options;
---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import "reflect-metadata";
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+import {
+  EntityManager,
+  EntityRepository,
+  MikroORM,
+  RequestContext,
+} from "@mikro-orm/core";
+
+import { Item } from "./entities/Item";
+
+dotenv.config();
+
+export const DI = {} as {
+  orm: MikroORM;
+  em: EntityManager;
+  itemRepository: EntityRepository<Item>;
+};
+
+const app = express();
+const port = 5000;
+
+const main = async () => {
+  DI.orm = await MikroORM.init();
+  DI.em = DI.orm.em;
+  DI.itemRepository = DI.orm.em.getRepository(Item);
+
+  app.use(express.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  app.use((_req, _res, next) => RequestContext.create(DI.orm.em, next));
+
+  app.get("/", (_, res) => {
+    res.send("Hello there");
+  });
+
+  app.listen(port, () => {
+    console.log(`Listenin at port ${port}`);
+  });
+};
+
+main().catch(console.error);
