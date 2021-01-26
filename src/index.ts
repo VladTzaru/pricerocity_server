915d4a2ad9b7f5d1ea985a6c5f4ce7d6b51cb653
VladTzaru <bvanja.87@gmail.com>
2021-01-26 15:34:08 +0100

    Added item controller

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
+import "reflect-metadata";
 import express from "express";
 import dotenv from "dotenv";
+
 import { MikroORM } from "@mikro-orm/core";
 import { MongoDriver } from "@mikro-orm/mongodb";
+import { MongoHighlighter } from "@mikro-orm/mongo-highlighter";
+
 import { Item } from "./entities/Item";
+
+const app = express();
+const port = 5000;
 
 const main = async () => {
   dotenv.config();
@@ -12,14 +19,13 @@ const main = async () => {
     entities: [Item],
     dbName: "pricerocityDB",
     type: "mongo",
+    highlighter: new MongoHighlighter(),
     clientUrl: process.env.MONGODB_URI,
+    debug: true,
   });
 
   await orm.em.getDriver().createCollections();
   // DB config ends
-
-  const app = express();
-  const port = 5000;
 
   app.get("/", (_, res) => {
     res.send("Hello there");
