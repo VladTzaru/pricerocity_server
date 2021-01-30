d7217ed894881d11af13fd4760c49a3246b38270
VladTzaru <bvanja.87@gmail.com>
2021-01-30 17:28:16 +0100

    Added BaseEntity to config

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,4 +1,4 @@
-import { Options } from "@mikro-orm/core";
+import { BaseEntity, Options } from "@mikro-orm/core";
 import { MongoHighlighter } from "@mikro-orm/mongo-highlighter";
 import { Buyer } from "./entities/Buyer";
 import { InvoiceR1 } from "./entities/InvoiceR1";
@@ -6,7 +6,7 @@ import { Item } from "./entities/Item";
 
 const options: Options = {
   type: "mongo",
-  entities: [Item, Buyer, InvoiceR1],
+  entities: [Item, Buyer, InvoiceR1, BaseEntity],
   clientUrl: process.env.MONGODB_URI,
   dbName: "pricerocityDB",
   highlighter: new MongoHighlighter(),
